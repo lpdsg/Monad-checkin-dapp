fe62c9b7a1a9354e57a9ce7e9070c32146038fac
PLLL <linaug999@163.com>
2026-07-14 17:49:10 +0800

    Implement check-in functionality and status check
    Added functions to check user check-in status and perform check-ins.

--- contracts/CheckIn.sol
+++ contracts/CheckIn.sol
@@ -21,5 +21,37 @@ contract CheckIn {
     function getCurrentDay() public view returns (uint256) {
         return block.timestamp / 1 days;
     }
+    
+    function hasCheckedInToday(address user) public view returns (bool) {
+    uint256 currentDay = getCurrentDay();
+
+    return checkInRecords[user].lastCheckInDay == currentDay;
+    }
+
+    function checkIn() public {
+    uint256 currentDay = getCurrentDay();
+    CheckInInfo storage userInfo = checkInRecords[msg.sender];
+
+    require(
+        userInfo.lastCheckInDay != currentDay,
+        "You have already checked in today"
+    );
+
+    if (userInfo.lastCheckInDay + 1 == currentDay) {
+        userInfo.currentStreak += 1;
+    } else {
+        userInfo.currentStreak = 1;
+    }
+
+    userInfo.totalCheckIns += 1;
+    userInfo.lastCheckInDay = currentDay;
+
+    emit CheckedIn(
+        msg.sender,
+        currentDay,
+        userInfo.totalCheckIns,
+        userInfo.currentStreak
+    );
+}
 
 }
